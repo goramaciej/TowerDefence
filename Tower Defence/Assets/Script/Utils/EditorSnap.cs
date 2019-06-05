38d941021d13f36af6e33d26bf465504f0d63091
maciejg <maciej.gora01@gmail.com>
2019-06-05 19:23:37 +0200

    Cube added editor script

---
+++ Tower Defence/Assets/Script/Utils/EditorSnap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] float gridSize = 10f;
+    Vector3 snapPos;
+
+    void Update()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = snapPos;
+
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = (snapPos.x / gridSize).ToString() + "," + (snapPos.z / gridSize).ToString();
+    }
+}
