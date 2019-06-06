88b021296ed996f43d3215e34d397e708ba44fce
maciejg <maciej.gora01@gmail.com>
2019-06-06 20:59:24 +0200

    Added editor script with waypoint separated from game mechanism

---
+++ Tower Defence/Assets/Script/EnemyMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> blockList;
+    [SerializeField] float duration = 1f;
+    [SerializeField] AnimationCurve curve = AnimationCurve.Linear(0.0f, 0.0f, 1.0f, 1.0f);
+
+    private int currentStep = 0;
+    private Vector3 currentStartPosition;
+    private Vector3 currentTargetPosition;
+    private float time = 0f;
+    private bool isAnimating = false;
+
+    // Start is called before the first frame update
+    void Start() {        
+        transform.position = blockList[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            StartCoroutine(FollowPath());
+        }
+
+        if (isAnimating) {
+            time += Time.deltaTime;
+            transform.position = Vector3.Lerp(currentStartPosition, currentTargetPosition, curve.Evaluate(time / duration));
+        }
+    }
+
+    IEnumerator FollowPath() {
+        isAnimating = true;
+        int count = blockList.Count;
+        for (int i=0; i< count; i++) {
+
+            currentStartPosition = blockList[i].transform.position;
+
+            if (i+1 == count) {
+                isAnimating = false;
+            } else {
+                currentTargetPosition = blockList[i + 1].transform.position;
+            }            
+            time = 0;
+            yield return new WaitForSeconds(duration);
+        }
+        //Debug.Log("coroutine end");
+    }
+
+   
+    
+}
--- Tower Defence/Assets/Script/Utils/EditorSnap.cs
+++ Tower Defence/Assets/Script/Utils/EditorSnap.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class EditorSnap : MonoBehaviour
 {
-    [SerializeField] float gridSize = 10f;
-    Vector3 snapPos;
+    
+     //waypoint;
+
+    private void Awake() {
+        Debug.Log("EditorSnapAwaken");
+        //Debug.Log("EditorSnap: " + waypoint);
+    }
 
     void Update()
-    {
+    {        
+        SnapToGrid();
+        UpdateLabel();
+    }
+
+    void SnapToGrid() {
+        Waypoint waypoint = GetComponent<Waypoint>();
+        Vector2Int waypointPosition = waypoint.GetPosition();
+        /*int gridSize = waypoint.GetGridSize();
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;*/
+        Vector3 snapPos = new Vector3(waypointPosition.x, 0f, waypointPosition.y);
         transform.position = snapPos;
+    }
 
+    void UpdateLabel() {
+        Waypoint waypoint = GetComponent<Waypoint>();
+        Vector2Int waypointPosition = waypoint.GetPosition();
+        int gridSize = waypoint.GetGridSize();
         TextMesh textMesh = GetComponentInChildren<TextMesh>();
-        textMesh.text = (snapPos.x / gridSize).ToString() + "," + (snapPos.z / gridSize).ToString();
+        textMesh.text = (waypointPosition.x / gridSize).ToString() + "," + (waypointPosition.y / gridSize).ToString();
+        gameObject.name = "Cube (" + textMesh.text + ")";
     }
 }
