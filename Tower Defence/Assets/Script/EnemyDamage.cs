40dad9feebedc726a84ed60b1d6bb76908aeaf2e
maciejg <maciej.gora01@gmail.com>
2019-07-09 08:34:13 +0200

    next

---
+++ Tower Defence/Assets/Script/EnemyDamage.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // non used temporarily
+    /*private BoxCollider boxCollider;
+    private MeshRenderer myRenderer;
+    private Bounds localBounds;*/
+
+    [SerializeField] int hitPoints = 100;
+
+    void Start()
+    {
+        //GetMyBounds();
+        //AddCollider();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other) {
+        TakeDamage();
+        if (hitPoints <= 0) {
+            KillEnemy();
+        }
+    }
+
+    private void TakeDamage() {
+        hitPoints--;
+    }
+    private void KillEnemy() {
+        Destroy(gameObject);
+    }
+
+
+
+    /*private void GetMyBounds() {
+        Quaternion currentRotation = this.transform.rotation;
+        this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        Bounds bounds = new Bounds(this.transform.position, Vector3.zero);
+
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>()) {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        Vector3 localCenter = bounds.center - this.transform.position;
+        bounds.center = localCenter;
+        localBounds = bounds;
+
+        this.transform.rotation = currentRotation;
+    }
+
+    private void AddCollider() {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+
+        if (!boxCollider) {
+            Debug.Log("No collider... add it");
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+        }
+        boxCollider.size = new Vector3(10f, 10f, 10f);
+
+        boxCollider.isTrigger = false;
+    }*/
+}
--- Tower Defence/Assets/Script/Tower.cs
+++ Tower Defence/Assets/Script/Tower.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemy;
+    [SerializeField] int attackRange = 30;
 
-    // Update is called once per frame
-    void Update()
-    {
-        objectToPan.LookAt(targetEnemy);
+    ParticleSystem.EmissionModule gunShooting;
+
+    private void Start() {
+        ParticleSystem gun = GetComponentInChildren<ParticleSystem>();
+        gunShooting = gun.emission;
+    }
+
+    void Update() {
+        FaceToEnemyAndShot();
+        
+    }
+    void FaceToEnemyAndShot() {
+        float currentDistance = Vector3.Distance(objectToPan.position, targetEnemy.position);
+
+        if ((currentDistance < attackRange) && targetEnemy) {
+            objectToPan.LookAt(targetEnemy);
+            if (!gunShooting.enabled) {
+                //gunShooting.enabled = true;
+            }
+        } else {
+            if (gunShooting.enabled) {
+                //gunShooting.enabled = false;
+            }
+        }
     }
 }
